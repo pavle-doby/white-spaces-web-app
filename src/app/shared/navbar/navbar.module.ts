a9d0908a753232e8366bc4ac3e9ce3feb0e71ec1
Pavle Marinkovic <pavle.marinkovic@elfak.rs>
2020-06-13 22:11:30 +0200

    feat(navbar): look and position

--- src/app/shared/navbar/navbar.module.ts
+++ src/app/shared/navbar/navbar.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FlexLayoutModule],
   exports: [NavbarComponent],
 })
 export class NavbarModule {}
