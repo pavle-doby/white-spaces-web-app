a92e4e9746b1be90d7ef033f24bfdcb8938ba369
Pavle Marinkovic <pavle.marinkovic@elfak.rs>
2020-07-22 09:45:14 +0200

    feat(checkout): generate components for checkout page

---
+++ src/app/shared/tabbar/tabbar.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TabbarComponent } from './tabbar.component';
+
+@NgModule({
+  declarations: [TabbarComponent],
+  imports: [CommonModule],
+  exports: [TabbarComponent],
+})
+export class TabbarModule {}
