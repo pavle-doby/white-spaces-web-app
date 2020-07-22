1b67bee0488cb67ede905d1d418e5795c058d861
Mladen Stojkovic <mladen.stojkovic@htecgroup.com>
2020-07-22 15:12:20 +0200

    feat(gradient): ghetto width height

--- src/app/shared/directives/gradient.directive.ts
+++ src/app/shared/directives/gradient.directive.ts
@@ -1,4 +1,10 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  Renderer2,
+  Input,
+} from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 
 @Directive({
@@ -9,9 +15,11 @@ import { DOCUMENT } from '@angular/common';
   ],
 })
 export class GradientDirective {
+  @Input() calculateByHeight: boolean = false;
   private documentWidth: number; //ceo width gradient 0 do 1
   private pageXOffset: number; //razdaljina trenutnog prozora od pocetka documentWidth. gradient 0+pageXOffset do 1.
   private currentPosition: number;
+  private elementDim: number;
   // x je objekat linear gradient 0-1
   //boja elementa je onda pageXOffset - 30 : x =documentWidth:(linear gradient ceo)
   constructor(
@@ -20,16 +28,13 @@ export class GradientDirective {
     private readonly document: Document,
     private renderer: Renderer2
   ) {
-    console.log(this.window.document.body.offsetWidth);
     this.documentWidth =
       this.window.document.body.offsetWidth - this.window.innerWidth;
-    //this.element.nativeElement.style.backgroundColor = 'yellow';
+    this.calculateFirstHalfGradient();
   }
 
   @HostListener('window:scroll', ['$event'])
   onWindowScroll($event) {
-    console.log(this.element.nativeElement.offsetHeight); //za navbar iteme je height u pitanju. Razmotri width
-    console.log(this.element);
     this.pageXOffset = this.window.pageXOffset;
     this.currentPosition = this.pageXOffset + this.window.innerWidth;
     console.log(this.pageXOffset);
@@ -44,28 +49,75 @@ export class GradientDirective {
   }
 
   private calculateFirstHalfGradient() {
+    this.elementDim = this.calculateByHeight
+      ? this.element.nativeElement.offsetHeight
+      : this.element.nativeElement.offsetWidth;
     const nula = 0;
     const full = this.documentWidth / 2;
-
+    let newStartRGB = [];
+    let newEndRGB = [];
     let gradientStartRGB = [242, 232, 220];
     let gradientEndRGB = [217, 183, 197];
+    let gradientFinalRGB = [15, 13, 59];
+
     let diffArray2 = gradientStartRGB.map(
       (value, i) => value - gradientEndRGB[i]
     );
+    let diffArray3 = gradientEndRGB.map(
+      (value, i) => value - gradientFinalRGB[i]
+    );
     let startPercentage =
-      (((this.currentPosition - 30) * 100) /
+      (((this.currentPosition - this.elementDim) * 100) /
         this.window.document.body.offsetWidth) *
       2;
     let endPercentage =
       ((this.currentPosition * 100) / this.window.document.body.offsetWidth) *
       2;
-    let newStartRGB = diffArray2.map((element) =>
-      Math.round((startPercentage * element) / 100)
-    );
-    let newEndRGB = diffArray2.map(
-      (element) => Math.round((endPercentage * element) / 100) + 1
-    );
-    console.log(newStartRGB, newEndRGB);
+
+    if (this.currentPosition - 1920 < this.documentWidth / 2) {
+      newStartRGB = diffArray2.map((element) =>
+        Math.round((startPercentage * element) / 100)
+      );
+      newEndRGB = diffArray2.map(
+        (element) => Math.round((endPercentage * element) / 100) + 1
+      );
+      this.renderer.setStyle(
+        this.element.nativeElement,
+        'background',
+        `linear-gradient(to right,
+      rgba(${242 - newStartRGB[0]}, ${232 - newStartRGB[1]}, ${
+          220 - newStartRGB[2]
+        }, 1) 0%,
+     rgba(${242 - newEndRGB[0]}, ${232 - newEndRGB[1]}, ${
+          220 - newEndRGB[2]
+        }, 1) 100%
+  )`
+      );
+    } else {
+      newStartRGB = diffArray3.map((element) =>
+        Math.round((startPercentage * element) / 200)
+      );
+      newEndRGB = diffArray3.map(
+        (element) => Math.round((endPercentage * element) / 200) + 1
+      );
+      console.log(newStartRGB);
+      console.log(newEndRGB);
+
+      this.renderer.setStyle(
+        this.element.nativeElement,
+        'background',
+        `linear-gradient(to right,
+      rgba(${217 - newStartRGB[0]}, ${183 - newStartRGB[1]}, ${
+          197 - newStartRGB[2]
+        }, 1) 0%,
+     rgba(${217 - newEndRGB[0]}, ${183 - newEndRGB[1]}, ${
+          197 - newEndRGB[2]
+        }, 1) 100%
+  )`
+      );
+    }
+
+    //console.log(newStartRGB, newEndRGB);
     // this.element.nativeElement.style.background = `linear-gradient(90deg,
     //      rgba(${242 - newStartRGB[0]}, ${232 - newStartRGB[1]}, ${
     //   220 - newStartRGB[2]
@@ -74,18 +126,6 @@ export class GradientDirective {
     //   220 - newEndRGB[2]
     // }, 1) 100%,
     //  )`;
-    this.renderer.setStyle(
-      this.element.nativeElement,
-      'background',
-      `linear-gradient(to right,
-    rgba(${242 - newStartRGB[0]}, ${232 - newStartRGB[1]}, ${
-        220 - newStartRGB[2]
-      }, 1) 0%,
-   rgba(${242 - newEndRGB[0]}, ${232 - newEndRGB[1]}, ${
-        220 - newEndRGB[2]
-      }, 1) 100%
-)`
-    );
 
     //this.currentPos-30 : x = this.width/2 : 100
     //x = (this.currentPos-30*100)/this.width/2
--- src/app/shared/side-card-packages/side-card-packages.module.ts
+++ src/app/shared/side-card-packages/side-card-packages.module.ts
@@ -4,6 +4,7 @@ import { SideCardPackagesComponent } from './side-card-packages.component';
 import { SideCardModule } from '../side-card/side-card.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { SideCardPackagesBoxModule } from './side-card-packages-box/side-card-packages-box.module';
+import { GradientDirectiveModule } from '../directives/gradient.directive.module';
 
 @NgModule({
   declarations: [SideCardPackagesComponent],
@@ -12,6 +13,7 @@ import { SideCardPackagesBoxModule } from './side-card-packages-box/side-card-pa
     SideCardModule,
     FlexLayoutModule,
     SideCardPackagesBoxModule,
+    GradientDirectiveModule,
   ],
   exports: [SideCardPackagesComponent],
 })
