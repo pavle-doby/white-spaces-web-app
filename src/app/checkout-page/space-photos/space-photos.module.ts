8fab00ac3dfa8fdaa0d2b1af90f970bcc065eeca
Pavle Marinkovic <pavle.marinkovic@elfak.rs>
2020-07-23 00:20:23 +0200

    feat(checkout): layout center

--- src/app/checkout-page/space-photos/space-photos.module.ts
+++ src/app/checkout-page/space-photos/space-photos.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { SpacePhotosComponent } from './space-photos.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [SpacePhotosComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FlexLayoutModule],
   exports: [SpacePhotosComponent],
 })
 export class SpacePhotosModule {}
