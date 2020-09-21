d98ee67b11594f4e34f23e57b5d4844ae26121ae
Pavle Marinkovic <pavle.marinkovic@elfak.rs>
2020-09-21 14:52:22 +0200

    feat: about

--- src/app/shared/side-card-about/side-card-about.module.ts
+++ src/app/shared/side-card-about/side-card-about.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { SideCardAboutComponent } from './side-card-about.component';
 import { SideCardModule } from '../side-card/side-card.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { GradientDirectiveModule } from '../directives/gradient.directive.module';
 
 @NgModule({
   declarations: [SideCardAboutComponent],
-  imports: [CommonModule, SideCardModule, FlexLayoutModule],
+  imports: [
+    CommonModule,
+    SideCardModule,
+    FlexLayoutModule,
+    GradientDirectiveModule,
+  ],
   exports: [SideCardAboutComponent],
 })
 export class SideCardAboutModule {}
