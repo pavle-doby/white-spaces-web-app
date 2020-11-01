fa1c4af4d5e171dbece4e3864818b1dc769511f1
Pavle <limuun23@gmail.com>
2020-11-02 00:15:10 +0100

    feat(chekcout-little-things): sort add on list

--- src/app/checkout-page/add-on-list/add-on-list.component.ts
+++ src/app/checkout-page/add-on-list/add-on-list.component.ts
@@ -63,26 +63,32 @@ export class AddOnListComponent implements OnInit {
           LocalStorageService.Instance.AddOnCategroyId = addOnList?.length
             ? addOnList[0].category_id
             : null;
-          this.addOnList = addOnList.map((addOnDTO) => {
-            const questions = convertQuestionsDTOListToQuestionsList(
-              addOnDTO.additional_data.questions,
-              addOnDTO
-            );
-            return new AddOn({
-              id: addOnDTO.id,
-              name: addOnDTO.name,
-              description: addOnDTO.data?.description,
-              price: addOnDTO.price,
-              isSelected: false,
-              questions,
-            });
-          });
+          this.addOnList = addOnList
+            .map((addOnDTO) => {
+              const questions = convertQuestionsDTOListToQuestionsList(
+                addOnDTO.additional_data.questions,
+                addOnDTO
+              );
+              return new AddOn({
+                id: addOnDTO.id,
+                name: addOnDTO.name,
+                description: addOnDTO.data?.description,
+                price: addOnDTO.price,
+                isSelected: false,
+                questions,
+              });
+            })
+            .sort(this.compareAddOns);
 
           this.$store.dispatch(
             setAddOnListCheckout({ addOnList: this.addOnList })
           );
         });
     }
+  }
+
+  public compareAddOns(a: AddOn, b: AddOn): number {
+    return a.name.localeCompare(b.name);
   }
 
   public onAddRemoveAddOn(addOn: AddOn): void {
