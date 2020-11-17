d764a7a8c2f998602dfca501f41c6d92d57bc99d
Pavle <limuun23@gmail.com>
2020-11-17 20:59:59 +0100

    feat(proper-text): checkout mobile page, upload info tooltip

--- src/app/shared/upload/upload.module.ts
+++ src/app/shared/upload/upload.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { UploadComponent } from './upload.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { GradientDirectiveModule } from '../directives/gradient.directive.module';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
 
+const MaterialModules = [MatTooltipModule, MatIconModule];
 @NgModule({
   declarations: [UploadComponent],
-  imports: [CommonModule, FlexLayoutModule, GradientDirectiveModule],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    GradientDirectiveModule,
+    MaterialModules,
+  ],
   exports: [UploadComponent],
 })
 export class UploadModule {}
