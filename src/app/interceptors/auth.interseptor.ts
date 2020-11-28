fbeee814d2f9eb8383624ad9e72cc07a7a88cd89
Pavle Marinkovic <pavle.marinkovic@elfak.rs>
2020-11-29 00:17:30 +0100

    feat(auth-token): response UNAUTHORIZED handle

--- src/app/interceptors/auth.interseptor.ts
+++ src/app/interceptors/auth.interseptor.ts
@@ -4,11 +4,16 @@ import {
   HttpHandler,
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs/operators';
+import HttpStatusCode from 'src/models/HttpStatusCode';
+import { MainRouterPaths } from 'src/models/MainRouterPaths.model';
+import { LoginParam } from '../app.config';
 import { LocalStorageService } from '../services/local-storage.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor() {}
+  constructor(private readonly router: Router) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     const authToken = LocalStorageService.Instance.AuthToken;
@@ -17,6 +22,17 @@ export class AuthInterceptor implements HttpInterceptor {
       headers: req.headers.set('Authorization', authToken),
     });
 
-    return next.handle(authReq);
+    return next.handle(authReq).pipe(
+      tap(
+        (event) => event,
+        (error) => {
+          if (error.status === HttpStatusCode.UNAUTHORIZED) {
+            LocalStorageService.Instance.clearAuthToken();
+            const logInRoute = `/${MainRouterPaths.LOGIN}?login=${LoginParam.LOGIN}`;
+            this.router.navigateByUrl(logInRoute);
+          }
+        }
+      )
+    );
   }
 }
