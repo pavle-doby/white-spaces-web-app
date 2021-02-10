02f2b5e4f8c106632524fae690eb201253f26ca4
Pavle Marinkovic <pavle.marinkovic@elfak.rs>
2021-02-11 00:39:52 +0100

    fix: blog scroll

--- src/app/blog-page/blog-page.component.ts
+++ src/app/blog-page/blog-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { throttleTime } from 'rxjs/internal/operators/throttleTime';
+import { Subject } from 'rxjs/internal/Subject';
+import { SCROLL_BEHAVIOR, SCROLL_SPEED } from '../app.config';
 import { AdminService } from '../services/admin.service';
 import { isHandset } from '../shared/Utilities';
 
@@ -13,6 +16,13 @@ export class BlogPageComponent implements OnInit {
   public ready: boolean = false;
 
   public isHandset: boolean = isHandset();
+
+  //scroll
+  public scrollLeftSub: Subject<void> = new Subject<void>();
+  public scrollRightSub: Subject<void> = new Subject<void>();
+
+  private throttleDuration: number = 500; // in ms
+  private scrollOffset = 500; // in px
 
   constructor(private window: Window, private adminService: AdminService) {
     this.window.document.body.style.width = 'auto';
@@ -41,5 +51,67 @@ export class BlogPageComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.scrollRightSub
+      .pipe(throttleTime(this.throttleDuration))
+      .subscribe(() => {
+        this.scrollRight();
+      });
+
+    this.scrollLeftSub
+      .pipe(throttleTime(this.throttleDuration))
+      .subscribe(() => {
+        this.scrollLeft();
+      });
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    switch (event.key) {
+      case 'PageDown':
+      case 'ArrowDown':
+      case 'ArrowRight': {
+        this.scrollRightSub.next();
+        break;
+      }
+      case 'PageUp':
+      case 'ArrowUp':
+      case 'ArrowLeft': {
+        this.scrollLeftSub.next();
+        break;
+      }
+      default: {
+        break;
+      }
+    }
+  }
+
+  scrollRight(): void {
+    window.scrollBy({
+      left: this.scrollOffset,
+      top: 0,
+      behavior: SCROLL_BEHAVIOR,
+    });
+  }
+
+  scrollLeft(): void {
+    window.scrollBy({
+      left: -this.scrollOffset,
+      top: 0,
+      behavior: SCROLL_BEHAVIOR,
+    });
+  }
+
+  // public onMouseWheel($event: WheelEvent): void {
+  //   const x = $event.deltaX || $event.deltaY;
+  //   const goRight = x > 0;
+
+  //   if (goRight) {
+  //     this.scrollRightSub.next();
+  //   } else {
+  //     this.scrollLeftSub.next();
+  //   }
+
+  //   console.log('scroll blog');
+  // }
 }
